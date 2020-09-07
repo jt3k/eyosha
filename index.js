1b684f0798c1c7de899bff4153c09371040b2e12
Andrey Gurtovoy <iyntx@ya.ru>
2020-09-07 19:25:13 +0300

    implemented

---
+++ index.js
@@ -0,0 +1,20 @@
+const Eyo = require('eyo-kernel');
+const { Composer } = require('micro-bot');
+
+const bot = new Composer();
+
+const safeEyo = new Eyo();
+safeEyo.dictionary.loadSafeSync();
+
+bot.on('message', ({ reply, message }) => {
+  const cases = safeEyo.lint(message.text).map(({ after }) => '«' + after + '»');
+  if (cases.length) {
+    return reply(
+      `${cases.length === 1 ? 'Слово' : 'Слова'}: ${cases.join(', ')} ${
+        cases.length === 1 ? 'пишется' : 'пишутся'
+      } не так :)`
+    );
+  }
+});
+
+module.exports = bot;

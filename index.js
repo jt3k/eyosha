5f64669658efed791fc76809c74c718ff33db610
Andrey Gurtovoy <iyntx@ya.ru>
2022-05-05 20:02:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const safeEyo = new Eyo();
 safeEyo.dictionary.loadSafeSync();
 
 bot.on('message', ({ reply, message }) => {
-  const cases = safeEyo.lint(message.text).map(({ after }) => '«' + after + '»');
+  const cases = safeEyo.lint(message.text).map(({ after }) => '«' + after + '»')
+  // Remove duplicates
+  .filter((item, index, self) => self.indexOf(item) === index);
   if (cases.length) {
     return reply(
       `${cases.length === 1 ? 'Слово' : 'Слова'}: ${cases.join(', ')} ${
